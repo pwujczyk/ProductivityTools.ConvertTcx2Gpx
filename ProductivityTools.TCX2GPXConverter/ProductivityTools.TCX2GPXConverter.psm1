58540237b7fa0694bff3c7ee2980726e1226708b
Pawel Wujczyk <pwujczyk@hotmail.com>
2021-01-12 22:17:53 +0100

    converT

--- ProductivityTools.TCX2GPXConverter/ProductivityTools.TCX2GPXConverter.psm1
+++ ProductivityTools.TCX2GPXConverter/ProductivityTools.TCX2GPXConverter.psm1
@@ -6,15 +6,61 @@ function ValidateGPSBabel
 	$x=Get-ChildItem GPSBabel -ErrorAction Ignore
 	if ($x -eq $null)
 	{
+		Write-Verbose "Extracting GPS Babel";
 		Expand-7Zip -ArchiveFileName .\GPSBabel.7z -TargetPath GPSBabel
 	}	
+	else
+	{
+		Write-Verbose "GPS babel already extracted.";
+	}
+}
+
+function ConvertTcX(){
+	[Cmdletbinding()]
+	param(
+		[string]$Path
+	)
+	
+	Write-Verbose "Convert $Path"
+	
+	$targetFile=$Path -replace ".tcx", ".gpx"
+	
+	$targetFileExists=Get-ChildItem $targetFile -ErrorAction Ignore
+	if($targetFileExists -ne $null)
+	{
+		throw "Target file $targetFile exists."
+	}
+	
+
+	$fileExe = "$PSScriptRoot\GPSBabel\gpsbabel.exe"
+	& $fileExe  -i gtrnctr -f $Path -o gpx -F $targetFile	
+}
+
+function Convert {
+	[Cmdletbinding()]
+	param(
+		[string]$Path
+	)
+
+	$tcxs=Get-ChildItem -Path $Path -Filter *.tcx
+	foreach($tcx in $tcxs)
+	{
+		ConvertTcx($tcx.FullName)
+	}
 }
 
 
 function Dof {
 	[Cmdletbinding()]
-	param()
+	param(
+		[string]$Path
+	)
+	
 	
 	Write-Output "Hello";
 	Write-Verbose "Hello";
+	
+	ValidateGPSBabel
+	
+	Convert($Path)
 }
--- ProductivityTools.TCX2GPXConverter/Runner.ps1
+++ ProductivityTools.TCX2GPXConverter/Runner.ps1
@@ -2,4 +2,4 @@ clear
 cd $PSScriptRoot
 Import-Module .\ProductivityTools.TCX2GPXConverter.psm1 -Force 
 
-Dof -Verbose 
+Dof -Path "c:\Users\pwujczyk\Desktop\endomondo-2020-12-08 - Copy\Workouts\" -Verbose 
