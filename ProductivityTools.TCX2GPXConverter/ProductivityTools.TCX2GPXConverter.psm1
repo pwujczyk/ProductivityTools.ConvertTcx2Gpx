9b2730191c5a66532b436559e1dba9ad3ff8a0dc
Pawel Wujczyk <pwujczyk@hotmail.com>
2021-01-12 21:54:29 +0100

    extract gps babel

--- ProductivityTools.TCX2GPXConverter/ProductivityTools.TCX2GPXConverter.psm1
+++ ProductivityTools.TCX2GPXConverter/ProductivityTools.TCX2GPXConverter.psm1
@@ -1,6 +1,16 @@
-<#
-	My Function
-#>
+function ValidateGPSBabel
+{
+	[Cmdletbinding()]
+	param()
+	
+	$x=Get-ChildItem GPSBabel -ErrorAction Ignore
+	if ($x -eq $null)
+	{
+		Expand-7Zip -ArchiveFileName .\GPSBabel.7z -TargetPath GPSBabel
+	}	
+}
+
+
 function Dof {
 	[Cmdletbinding()]
 	param()
